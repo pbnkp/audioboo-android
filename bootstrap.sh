3b51198e419837ab125d3529a99198969664878f
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-03 17:21:19 +0000

    AudioBoo:   Build libflag as a native lib
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@833 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ bootstrap.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+#
+# This file is part of AudioBoo, an android program for audio blogging.
+# Copyright (C) 2009 BestBefore Media Ltd. All rights reserved.
+#
+# Author: Jens Finkhaeuser <jens@finkhaeuser.de>
+#
+# $Id$
+
+# 1. Make sure we're in the project root. We'll look for AndroidManifest.xml,
+#    and check whether that includes a reference to AudioBoo.
+PROJECT_PATH=$(pwd)
+MANIFEST="${PROJECT_PATH}/AndroidManifest.xml"
+if [ ! -f "${MANIFEST}" ] ; then
+  echo "No AndroidManifest.xml file found!" >&2
+  echo "You need to run this script in the project root." >&2
+  exit 1
+fi
+
+if grep -qi audioboo "${MANIFEST}" ; then
+  true
+else
+  echo "No mention of AudioBoo found in AndroidManifest.xml!" >&2
+  echo "You need to run this script in the project root." >&2
+  exit 2
+fi
+
+# 2. Make sure that there's an NDK link in the project dire
+NDK_LINK="${PROJECT_PATH}/ndk"
+if [ ! -L "${NDK_LINK}" ] ; then
+  echo "Please create a symbolic link to the Android NDK in the project root," >&2
+  echo "i.e. run: ln -snf <ndk path> ndk" >&2
+  exit 3
+fi
+
+# 3. In the NDK's directory, create symbolic links back to this project.
+ln -snf "${PROJECT_PATH}/native/src" "${NDK_LINK}/sources/AudioBoo"
+ln -snf "${PROJECT_PATH}/native/apps/debug" "${NDK_LINK}/apps/AudioBoo-debug"
+ln -snf "${PROJECT_PATH}/native/apps/release" "${NDK_LINK}/apps/AudioBoo-release"
+
+# 4. Create Application.mk in the apps directories.
+for target in debug release ; do
+  cat "${PROJECT_PATH}/native/apps/Application.mk.in" | \
+    sed -e "s;@PATH@;${PROJECT_PATH};g" \
+        -e "s;@OPTIMIZATION@;${target};g" \
+        >"${PROJECT_PATH}/native/apps/${target}/Application.mk"
+done
+
+# 5. Build native stuff. By default, build the release version, but if the
+#    a command line target "debug" is provided, build the debug version
+BUILD_TARGET="release"
+if [ ! -z "$1" ] ; then
+  if [ "$1" = "debug" ] ; then
+    BUILD_TARGET="debug"
+  elif [ "$1" = "release" ] ; then
+    true
+  else
+    echo "The only acceptable parameters for this script are 'debug' or 'release'" >&2
+    exit 4
+  fi
+fi
+cd "${NDK_LINK}" && make APP="AudioBoo-${BUILD_TARGET}" V=1
+cd "${PROJECT_PATH}"
+
---
+++ native/src/config/ogg/config_types.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+
+/* These would normally be filled in by configure, but we're just hardcoding
+ * them here. */
+typedef int16_t ogg_int16_t;
+typedef u_int16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef u_int32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+
+#endif
