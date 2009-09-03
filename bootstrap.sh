7f2a1588c7c8adec492110e30a48e26aed1f5997
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-03 20:03:09 +0000

    AudioBoo:   Split native build into two separate parts so not everything has to be   rebuilt every time
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@836 e6edf6fb-f266-4316-afb4-e53d95876a76

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,17 +48,4 @@ done
 
 # 5. Build native stuff. By default, build the release version, but if the
 #    a command line target "debug" is provided, build the debug version
-BUILD_TARGET="release"
-if [ ! -z "$1" ] ; then
-  if [ "$1" = "debug" ] ; then
-    BUILD_TARGET="debug"
-  elif [ "$1" = "release" ] ; then
-    true
-  else
-    echo "The only acceptable parameters for this script are 'debug' or 'release'" >&2
-    exit 4
-  fi
-fi
-cd "${NDK_LINK}" && make APP="AudioBoo-${BUILD_TARGET}" V=1
-cd "${PROJECT_PATH}"
-
+./build-native.sh "$@"
