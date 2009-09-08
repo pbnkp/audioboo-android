489589700a1e996828f16052a116b2394f1028d3
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-08 12:41:58 +0000

    AudioBoo:   Add ToggleButton extension that notifies when its isPressed() state changes
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@856 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/fm/audioboo/widget/NotifyingToggleButton.java
@@ -0,0 +1,91 @@
+/**
+ * This file is part of AudioBoo, an android program for audio blogging.
+ * Copyright (C) 2009 BestBefore Media Ltd. All rights reserved.
+ *
+ * Author: Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * $Id$
+ **/
+
+package fm.audioboo.widget;
+
+import android.widget.ToggleButton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import android.util.Log;
+
+/**
+ * Simple extension to ToggleButton; allows attaching a listener to be notified
+ * when isPressed() changes.
+ **/
+public class NotifyingToggleButton extends ToggleButton
+{
+  /***************************************************************************
+   * Private constants
+   **/
+  // Log ID
+  private static final String LTAG  = "NotifyingToggleButton";
+
+
+  /***************************************************************************
+   * Notification class
+   **/
+  public static abstract class OnPressedListener
+  {
+    abstract void onPressed(NotifyingToggleButton button, boolean isPressed);
+  }
+
+
+  /***************************************************************************
+   * Data members
+   **/
+  private OnPressedListener mListener;
+  private boolean           mOldPressed;
+
+
+  /***************************************************************************
+   * Implementation
+   **/
+  public NotifyingToggleButton(Context context)
+  {
+    super(context);
+  }
+
+
+
+  public NotifyingToggleButton(Context context, AttributeSet attrs)
+  {
+    super(context, attrs);
+  }
+
+
+
+  public NotifyingToggleButton(Context context, AttributeSet attrs, int defStyle)
+  {
+    super(context, attrs, defStyle);
+  }
+
+
+
+  public void setOnPressedListener(OnPressedListener listener)
+  {
+    mListener = listener;
+  }
+
+
+
+  @Override
+  protected void drawableStateChanged()
+  {
+    super.drawableStateChanged();
+
+    if (mOldPressed != isPressed()) {
+      mOldPressed = isPressed();
+      if (null != mListener) {
+        mListener.onPressed(this, isPressed());
+      }
+    }
+  }
+}
