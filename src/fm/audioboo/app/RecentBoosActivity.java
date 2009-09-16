218b4c0a201d23f9723c38b4504d2f3d434a58b5
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-16 16:55:30 +0000

    AudioBoo:   Add refresh action to recent boos activity
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@912 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/fm/audioboo/app/RecentBoosActivity.java
+++ src/fm/audioboo/app/RecentBoosActivity.java
@@ -20,12 +20,15 @@ import android.content.res.Configuration;
 
 import android.view.View;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
 import java.util.LinkedList;
 
 import android.util.Log;
 
 /**
- * FIXME
+ * The RecentBoosActivity loads recent boos and displays them in a ListView.
  **/
 public class RecentBoosActivity extends ListActivity
 {
@@ -34,6 +37,10 @@ public class RecentBoosActivity extends ListActivity
    **/
   // Log ID
   private static final String LTAG  = "RecentBoosActivity";
+
+  // Action identifiers -- must correspond to the indices of the array
+  // "recent_boos_actions" in res/values/localized.xml
+  private static final int  ACTION_REFRESH  = 0;
 
 
   /***************************************************************************
@@ -75,12 +82,26 @@ public class RecentBoosActivity extends ListActivity
     super.onResume();
     Log.d(LTAG, "Resume");
 
-    // FIXME only if no boos have been loaded yet.
-    if (null != mBoos) {
-      return;
+    // Load boos, but only if that hasn't happened yet..
+    if (null == mBoos) {
+      refreshBoos();
     }
+  }
 
-    // Start loading recent Boos.
+
+
+  @Override
+  public void onConfigurationChanged(Configuration config)
+  {
+    // Ignore when the keyboard opens to the extent that we don't fetch boos
+    // again.
+    super.onConfigurationChanged(config);
+  }
+
+
+
+  private void refreshBoos()
+  {
     if (null == mApi) {
       mApi = new API();
     }
@@ -101,16 +122,6 @@ public class RecentBoosActivity extends ListActivity
 
 
 
-  @Override
-  public void onConfigurationChanged(Configuration config)
-  {
-    // Ignore when the keyboard opens to the extent that we don't fetch boos
-    // again.
-    super.onConfigurationChanged(config);
-  }
-
-
-
   private void onReceiveRecentBoos(BooList boos)
   {
     Log.d(LTAG, "Got response!");
@@ -126,5 +137,43 @@ public class RecentBoosActivity extends ListActivity
 
   private void onRecentBoosError(int code, String msg)
   {
+    // FIXME add error view to layout and display that.
+  }
+
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    String[] menu_titles = getResources().getStringArray(R.array.recent_boos_actions);
+    final int[] menu_icons = {
+      R.drawable.ic_menu_refresh,
+    };
+    assert(menu_icons.length == menu_titles.length);
+
+    for (int i = 0 ; i < menu_titles.length ; ++i) {
+      menu.add(0, i, 0, menu_titles[i]).setIcon(menu_icons[i]);
+    }
+    return true;
+  }
+
+
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    switch (item.getItemId()) {
+      case ACTION_REFRESH:
+        // Refresh Boos! While we do that, we want to empty the listview
+        setListAdapter(null);
+        refreshBoos();
+        break;
+
+      default:
+        // FIXME error toast
+        return false;
+    }
+
+    return true;
   }
 }
