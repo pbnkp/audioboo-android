3074b798fe1247cb56a21a29116aec91112ec1da
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-18 13:04:43 +0000

    AudioBoo:   Implements the glue between ImageCache and RecentBoosActivity that triggers   downloads into the cache, and handles responses from the cache.
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@921 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/fm/audioboo/app/BooListAdapter.java
+++ src/fm/audioboo/app/BooListAdapter.java
@@ -11,13 +11,25 @@ package fm.audioboo.app;
 
 import android.widget.BaseAdapter;
 
-import android.app.Activity;
+import android.app.ListActivity;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 
 import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.AbsListView;
+
+import android.net.Uri;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+
+import android.os.Handler;
+import android.os.Message;
+
+import java.util.LinkedList;
 
 import android.util.Log;
 
@@ -34,22 +46,72 @@ public class BooListAdapter extends BaseAdapter
 
 
   /***************************************************************************
+   * Helper class. Makes the BooListAdapter aware of relevant changes in
+   * the scroll state of the ListView it's filling, so it can trigger the
+   * or cancel heavy lifiting such as downloading images.
+   **/
+  public static class ScrollListener implements AbsListView.OnScrollListener
+  {
+    private BooListAdapter  mAdapter;
+
+    private boolean         mSentInitial = false;
+    private int             mFirst;
+    private int             mCount;
+
+    public ScrollListener(BooListAdapter adapter)
+    {
+      mAdapter = adapter;
+    }
+
+
+
+    public void onScroll(AbsListView view, int firstVisibleItem,
+        int visibleItemCount, int totalItemCount)
+    {
+      mFirst = firstVisibleItem;
+      mCount = visibleItemCount;
+
+      if (!mSentInitial && visibleItemCount > 0) {
+        mSentInitial = true;
+        mAdapter.startHeavyLifting(mFirst, mCount);
+      }
+    }
+
+
+
+    public void onScrollStateChanged(AbsListView view, int scrollState)
+    {
+      if (SCROLL_STATE_IDLE != scrollState) {
+        mAdapter.cancelHeavyLifting();
+        return;
+      }
+
+      mAdapter.startHeavyLifting(mFirst, mCount);
+    }
+  }
+
+
+  /***************************************************************************
    * Data
    **/
   // Boo data
-  private BooList   mBoos;
+  private BooList       mBoos;
 
   // Layout for the individual boos
-  private int       mBooLayoutId;
+  private int           mBooLayoutId;
 
   // Calling Activity
-  private Activity  mActivity;
+  private ListActivity  mActivity;
+
+  // Image dimensions. XXX The (reasonably safe) assumption is that in the view
+  // this adapter fills, all Boo images are to be displayed at the same size.
+  private int           mDimensions = -1;
 
 
   /***************************************************************************
    * Implementation
    **/
-  public BooListAdapter(Activity activity, int booLayoutId, BooList boos)
+  public BooListAdapter(ListActivity activity, int booLayoutId, BooList boos)
   {
     mActivity = activity;
     mBooLayoutId = booLayoutId;
@@ -75,7 +137,10 @@ public class BooListAdapter extends BaseAdapter
       view.setBackgroundResource(R.drawable.recent_boos_background_even);
     }
 
+    // Set the view's tag to the Boo we want to display. This is for later
+    // identification.
     Boo boo = mBoos.mClips.get(position);
+    view.setTag(boo);
 
     // Fill view with data.
     if (null != boo.mUser) {
@@ -97,7 +162,37 @@ public class BooListAdapter extends BaseAdapter
       }
     }
 
-    // TODO add more
+    // If the image cache contains an appropriate image at the right size, then
+    // we'll display that. If not, display a default image. We need to do the
+    // second in case the item view is being reused.
+    ImageView image_view = (ImageView) view.findViewById(R.id.recent_boos_item_image);
+    if (null != image_view) {
+      // First, determine the url we want to display.
+      Uri image_url = getDisplayUrl(boo);
+
+      boolean customImageSet = false;
+      if (null != image_url) {
+        // If we don't know the image dimensions yet, determine them now.
+        if (-1 == mDimensions) {
+          mDimensions = image_view.getLayoutParams().width
+            - image_view.getPaddingLeft() - image_view.getPaddingRight();
+        }
+
+        // Next, try to grab an image from the cache.
+        Bitmap bitmap = Globals.get().mImageCache.get(image_url, mDimensions);
+
+        if (null != bitmap) {
+          image_view.setImageDrawable(new BitmapDrawable(bitmap));
+          customImageSet = true;
+        }
+      }
+
+      // If we were not able to set a custom image here, default to the
+      // anonymous_boo one.
+      if (!customImageSet) {
+        image_view.setImageResource(R.drawable.anonymous_boo);
+      }
+    }
 
     return view;
   }
@@ -121,5 +216,107 @@ public class BooListAdapter extends BaseAdapter
   public Object getItem(int position)
   {
     return mBoos.mClips.get(position);
+  }
+
+
+
+  private void cancelHeavyLifting()
+  {
+    Globals.get().mImageCache.cancelFetching();
+  }
+
+
+
+  private void startHeavyLifting(int first, int count)
+  {
+    // Log.d(LTAG, "Downloads for items from " + first + " to " + (first + count));
+
+    // Prepare the list of uris to download.
+    LinkedList<ImageCache.CacheItem> uris = new LinkedList<ImageCache.CacheItem>();
+    for (int i = 0 ; i < count ; ++i) {
+      int index = first + i;
+      Boo boo = mBoos.mClips.get(index);
+      Uri uri = getDisplayUrl(boo);
+
+      if (null == uri) {
+        continue;
+      }
+
+      uris.add(new ImageCache.CacheItem(index, i, uri, mDimensions));
+    }
+
+    if (0 < uris.size()) {
+      // Use the cache to fetch these images.
+      Globals.get().mImageCache.fetch(uris, new Handler(new Handler.Callback() {
+        public boolean handleMessage(Message msg)
+        {
+          ImageCache.CacheItem item = (ImageCache.CacheItem) msg.obj;
+          switch (msg.what) {
+            case ImageCache.MSG_OK:
+              onCacheItemFetched(item);
+              break;
+
+            case ImageCache.MSG_CANCELLED:
+              //Log.d(LTAG, "Download of image cancelled: " + item.mImageUri);
+              break;
+
+            case ImageCache.MSG_ERROR:
+            default:
+              Log.e(LTAG, "Error fetching image at URL: " + item.mImageUri);
+              break;
+          }
+          return true;
+        }
+      }));
+    }
+  }
+
+
+
+  public void onCacheItemFetched(ImageCache.CacheItem item)
+  {
+    // Log.d(LTAG, "got results for : " + item.mImageUri);
+
+    // Right, we got an image. Now we just need to figure out the right view
+    // to go with it.
+    View item_view = mActivity.getListView().getChildAt(item.mViewIndex);
+    if (null == item_view) {
+      return;
+    }
+
+    // Make sure that the item for which the request was scheduled and the
+    // item we're displaying currently are the same. That's what we set the
+    // view's tag for in getView().
+    Boo expected_boo = mBoos.mClips.get(item.mItemIndex);
+    Boo current_boo = (Boo) item_view.getTag();
+    if (expected_boo.mId != current_boo.mId) {
+      // There's been a race between cancelling downloads and sending the
+      // message with the resulting bitmap.
+      return;
+    }
+
+    // Now display the image.
+    ImageView image_view = (ImageView) item_view.findViewById(R.id.recent_boos_item_image);
+    if (null == image_view) {
+      return;
+    }
+
+    image_view.setImageDrawable(new BitmapDrawable(item.mBitmap));
+  }
+
+
+
+  private Uri getDisplayUrl(Boo boo)
+  {
+    if (null == boo) {
+      return null;
+    }
+
+    Uri result = boo.mImageUrl;
+    if (null == result && null != boo.mUser) {
+      result = boo.mUser.mImageUrl;
+    }
+
+    return result;
   }
 }
