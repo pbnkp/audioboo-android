45540b7987b5822519f38d6a331fc9273ea46e10
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-08 16:37:40 +0000

    AudioBoo:   * Expecting serialized Boos now   * Skeleton code for uploading boos added
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@1009 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/fm/audioboo/app/PublishActivity.java
+++ src/fm/audioboo/app/PublishActivity.java
@@ -13,6 +13,13 @@ import android.app.Activity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import android.content.res.Configuration;
 
 import android.util.Log;
@@ -34,8 +41,15 @@ public class PublishActivity extends Activity
    * Public constants
    **/
   // Extra names
-  public static final String EXTRA_BOO  = "fm.audioboo.extras.boo";
+  public static final String EXTRA_BOO_FILENAME = "fm.audioboo.extras.boo-filename";
 
+
+  /***************************************************************************
+   * Data
+   **/
+  // Filename and Boo data.
+  private String  mBooFilename;
+  private Boo     mBoo;
 
 
   /***************************************************************************
@@ -46,8 +60,49 @@ public class PublishActivity extends Activity
   {
     super.onCreate(savedInstanceState);
 
+    // Grab extras
+    Bundle extras = getIntent().getExtras();
+    if (null == extras) {
+      throw new IllegalArgumentException("Intent needs to define the '"
+          + EXTRA_BOO_FILENAME + "' extra.");
+    }
+
+    // See if we've been passed a Media instance.
+    String filename = extras.getString(EXTRA_BOO_FILENAME);
+    Boo boo = Boo.constructFromFile(filename);
+    if (null == boo) {
+      throw new IllegalArgumentException("Boo file '" + filename + "' could "
+          + "not be loaded.");
+    }
+    mBoo = boo;
+    mBooFilename = filename;
+    Log.d(LTAG, "Boo: " + mBoo);
+
+
+    // Create view, etc.
     setContentView(R.layout.publish);
     setTitle(getResources().getString(R.string.publish_activity_title));
+
+    // Hook up the buttons.
+    Button submit = (Button) findViewById(R.id.publish_submit);
+    if (null != submit) {
+      submit.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v)
+        {
+          onSubmit();
+        }
+      });
+    }
+
+    ImageButton image = (ImageButton) findViewById(R.id.publish_image);
+    if (null != image) {
+      image.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v)
+        {
+          onEditImage();
+        }
+      });
+    }
   }
 
 
@@ -64,6 +119,8 @@ public class PublishActivity extends Activity
   public void onPause()
   {
     super.onPause();
+
+    // TODO write mBoo to mBooFilename. Let's us resume editing, to a degree.
   }
 
 
@@ -74,5 +131,22 @@ public class PublishActivity extends Activity
     // Ignore when the keyboard opens to the extent that we don't fetch boos
     // again.
     super.onConfigurationChanged(config);
+  }
+
+
+
+  private void onSubmit()
+  {
+    Log.d(LTAG, "Uploading...");
+
+    Globals.get().mAPI.uploadBoo(mBoo, null);
+    // FIXME
+  }
+
+
+
+  private void onEditImage()
+  {
+    Log.d(LTAG, "Editing image...");
   }
 }
