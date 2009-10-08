e822485a8833a828d5b72bdcb2191648b605970f
unwesen <unwesen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-08 16:36:15 +0000

    AudioBoo:   Add code for generating unique device IDs
    git-svn-id: https://imkons.jira.com/svn/ABOO/trunk@1008 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/fm/audioboo/app/Globals.java
+++ src/fm/audioboo/app/Globals.java
@@ -11,6 +11,14 @@ package fm.audioboo.app;
 
 import android.content.Context;
 
+import android.telephony.TelephonyManager;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+import java.io.FileInputStream;
+
+import java.util.StringTokenizer;
+
 import android.util.Log;
 
 /**
@@ -27,11 +35,23 @@ public class Globals
   // Maxim number of items we want in the image cache.
   private static final int        IMAGE_CACHE_MAX = 200;
 
+  // Defaults for the client ID.
+  private static final String     CLIENT_ID_PREFIX  = "android-";
+  private static final String     CLIENT_ID_UNKNOWN = "unknown-id";
+  private static final String     HARDWARE_UNKNOWN  = "unknown-hardware";
 
   /***************************************************************************
    * Singleton data
    **/
   private static Globals  sInstance;
+
+
+  /***************************************************************************
+   * Private instance data
+   **/
+  // Generated at startup; the algorithm always produces the same string for the
+  // same device.
+  public String       mClientID;
 
 
   /***************************************************************************
@@ -41,6 +61,7 @@ public class Globals
   public API          mAPI;
   public ImageCache   mImageCache;
   public BooPlayer    mPlayer;
+
 
 
   /***************************************************************************
@@ -100,5 +121,107 @@ public class Globals
     mPlayer.mShouldRun = false;
     mPlayer.interrupt();
     mPlayer = null;
+  }
+
+
+
+  /**
+   * Generates (and returns) mClientID
+   **/
+  public String getClientID()
+  {
+    if (null != mClientID) {
+      return mClientID;
+    }
+
+    TelephonyManager tman = (TelephonyManager) mContext.getSystemService(
+        Context.TELEPHONY_SERVICE);
+    String deviceId = tman.getDeviceId();
+
+    try {
+      // Let's be nice to our users and not send out their device ID in plain
+      // text.
+      MessageDigest m = MessageDigest.getInstance("SHA-1");
+      m.update((CLIENT_ID_PREFIX + getHardwareString() + deviceId).getBytes());
+      String digest = new BigInteger(1, m.digest()).toString(16);
+      while (digest.length() < 32) {
+        digest = "0" + digest;
+      }
+      mClientID = CLIENT_ID_PREFIX + digest;
+
+    } catch (java.security.NoSuchAlgorithmException ex) {
+      Log.e(LTAG, "ERROR: could not determine client ID: " + ex.getMessage());
+      // We'll have to set the client ID to something to avoid errors further
+      // down the line.
+      mClientID = CLIENT_ID_PREFIX + CLIENT_ID_UNKNOWN;
+    }
+
+    return mClientID;
+  }
+
+
+
+  /**
+   * Constructs a hardware identifier string from /proc/cpuinfo and returns it.
+   **/
+  private String getHardwareString()
+  {
+    // Read /proc/cpuinfo
+    String contents = null;
+    try {
+      FileInputStream is = new FileInputStream("/proc/cpuinfo");
+      contents = API.readStream(is);
+      try {
+        is.close();
+      } catch (java.io.IOException ex) {
+        // pass
+      }
+    } catch (java.io.FileNotFoundException ex) {
+      return HARDWARE_UNKNOWN;
+    } catch (java.io.IOException ex) {
+      return HARDWARE_UNKNOWN;
+    }
+
+    // The part to read is formatted like this:
+    // Hardware        : trout
+    // Revision        : 0080
+    // Serial          : 0000000000000000
+    boolean parse = false;
+    String hardware = null;
+    StringTokenizer st = new StringTokenizer(contents, "\n");
+    while (st.hasMoreTokens()) {
+      String line = st.nextToken();
+      if (line.startsWith("Hardware")) {
+        parse = true;
+      }
+
+      if (!parse) {
+        continue;
+      }
+
+      StringTokenizer lst = new StringTokenizer(line, ":");
+      if (2 > lst.countTokens()) {
+        Log.e(LTAG, "Cannot parse line: " + line);
+        continue;
+      }
+
+      // Skip first part.
+      lst.nextToken();
+      while (lst.hasMoreTokens()) {
+        String token = lst.nextToken().trim();
+        if (null == hardware) {
+          hardware = token;
+        }
+        else {
+          hardware += ":" + token;
+        }
+      }
+    }
+
+    if (null == hardware) {
+      hardware = HARDWARE_UNKNOWN;
+    }
+
+    return hardware;
   }
 }
