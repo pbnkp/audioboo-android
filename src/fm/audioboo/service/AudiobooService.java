c9e293ddc011ed77900828553f57496fc1434b34
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-02-26 14:17:26 +0000

    Implement AudiobooService by providing a player service implementation that wraps around BooPlayer

--- src/fm/audioboo/service/AudiobooService.java
+++ src/fm/audioboo/service/AudiobooService.java
@@ -15,14 +15,8 @@ import android.os.IBinder;
 
 import android.content.Intent;
 
-//import java.util.List;
-//import java.util.LinkedList;
-//import java.util.ArrayList;
-//
-//import de.finkhaeuser.dm.common.Intents;
-//import de.finkhaeuser.dm.common.Schemas;
-
 import fm.audioboo.data.BooData;
+import fm.audioboo.application.Boo;
 
 import android.util.Log;
 
@@ -39,6 +33,12 @@ public class AudiobooService extends Service
 
 
   /***************************************************************************
+   * Private data
+   **/
+  private BooPlayer   mPlayer;
+
+
+  /***************************************************************************
    * Service implementation
    **/
   @Override
@@ -51,6 +51,29 @@ public class AudiobooService extends Service
   }
 
 
+
+  @Override
+  public void onCreate()
+  {
+    if (null == mPlayer) {
+      mPlayer = new BooPlayer(this);
+      mPlayer.start();
+    }
+  }
+
+
+
+  @Override
+  public void onDestroy()
+  {
+    if (null != mPlayer) {
+      mPlayer.mShouldRun = false;
+      mPlayer.interrupt();
+      mPlayer = null;
+    }
+  }
+
+
   /***************************************************************************
    * IBooPlaybackService implementation
    **/
@@ -58,136 +81,36 @@ public class AudiobooService extends Service
   {
     public void play(BooData boo)
     {
-      // FIXME
+      mPlayer.play(new Boo(boo));
     }
 
 
 
     public void stop()
     {
-      // FIXME
+      mPlayer.stopPlaying();
     }
 
 
 
     public void pause()
     {
-      // FIXME
+      mPlayer.pausePlaying();
     }
 
 
 
     public void resume()
     {
-      // FIXME
+      mPlayer.resumePlaying();
     }
 
 
 
     public int getState()
     {
-      return 0; // FIXME
+      return mPlayer.getPlaybackState();
     }
-
-
-
-//    public void resolveDependencies(String packageName)
-//    {
-//      // Parse intents from the package.
-//      List<Intent> il = scanPackageForDependencies(packageName);
-//      if (null == il) {
-//        // Done. No dependencies are specified, or parsing failed.
-//        return;
-//      }
-//
-//      // Filter out resolvable intents
-//      il = removeResolvableIntents(il);
-//      if (null == il) {
-//        // Done. All dependencies are resolvable.
-//        return;
-//      }
-//
-//      // For the remaining intents, query the DependencyResolutionProvider for
-//      // possible matches.
-//      displayChoicesForIntents(il);
-//    }
-//
-//
-//
-//    public List<Intent> scanPackageForDependencies(String packageName)
-//    {
-//      return Intents.parseIntents(BooPlaybackServiceService.this, packageName);
-//    }
-//
-//
-//
-//    public List<Intent> removeResolvableIntents(List<Intent> intents)
-//    {
-//      if (null == intents || 0 >= intents.size()) {
-//        return null;
-//      }
-//
-//
-//      PackageManager pm = getPackageManager();
-//      LinkedList<Intent> results = new LinkedList<Intent>();
-//
-//      for (Intent i : intents) {
-//        String componentType = i.getStringExtra(
-//            Schemas.Client.EXTRA_COMPONENT_TYPE);
-//
-//        if (Schemas.Client.CT_RECEIVER.equals(componentType)) {
-//          // "receiver"
-//          List<ResolveInfo> info = pm.queryBroadcastReceivers(i, 0);
-//          if (null == info || 0 >= info.size()) {
-//            results.add(i);
-//          }
-//        }
-//
-//        else if (Schemas.Client.CT_SERVICE.equals(componentType)) {
-//          // "service"
-//          List<ResolveInfo> info = pm.queryIntentServices(i, 0);
-//          if (null == info || 0 >= info.size()) {
-//            results.add(i);
-//          }
-//        }
-//
-//        else {
-//          // "activity", default & lacuna
-//          List<ResolveInfo> info = pm.queryIntentActivities(i, 0);
-//          if (null == info || 0 >= info.size()) {
-//            results.add(i);
-//          }
-//        }
-//      }
-//
-//
-//      if (0 >= results.size()) {
-//        return null;
-//      }
-//      return results;
-//    }
-//
-//
-//
-//    public void displayChoicesForIntents(List<Intent> intents)
-//    {
-//      if (null == intents || 0 >= intents.size()) {
-//        Log.e(LTAG, "Intent list is empty.");
-//        return;
-//      }
-//
-//      // In order to serialize the intents list into a new Intent, we'll need
-//      // to create an ArrayList first.
-//      ArrayList<Intent> al = new ArrayList<Intent>(intents);
-//
-//      // Now create the Intent to launch ResolverActivity with, passing the
-//      // array list as an extra.
-//      Intent i = new Intent(BooPlaybackServiceService.this, ResolverActivity.class);
-//      i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//      i.putParcelableArrayListExtra(ResolverActivity.EXTRA_INTENTS, al);
-//
-//      startActivity(i);
-//    }
   };
 }
 
