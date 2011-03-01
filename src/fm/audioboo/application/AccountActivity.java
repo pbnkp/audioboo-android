42e7383ef08f73bc899a17ab62204a5a0e8ccf7f
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-03-01 16:49:45 +0000

    Bump copyright notice in comments

--- src/fm/audioboo/application/AccountActivity.java
+++ src/fm/audioboo/application/AccountActivity.java
@@ -1,6 +1,8 @@
 /**
  * This file is part of AudioBoo, an android program for audio blogging.
- * Copyright (C) 2009 BestBefore Media Ltd. All rights reserved.
+ * Copyright (C) 2009 BestBefore Media Ltd.
+ * Copyright (C) 2010,2011 AudioBoo Ltd.
+ * All rights reserved.
  *
  * Author: Jens Finkhaeuser <jens@finkhaeuser.de>
  *
