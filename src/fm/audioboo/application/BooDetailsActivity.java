fbdcee920deb1e1b5f69256d36dd1f3b8997c955
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-03-08 22:54:08 +0000

    Implement most of BooDetailsActivity; needs a few layouting changes and clicking on the user needs ot launch the user activity.

--- src/fm/audioboo/application/BooDetailsActivity.java
+++ src/fm/audioboo/application/BooDetailsActivity.java
@@ -12,15 +12,25 @@ package fm.audioboo.application;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 import android.content.Intent;
-
 import android.content.res.Configuration;
-import android.content.res.TypedArray;
 
-import android.widget.TabHost;
+import android.net.Uri;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import android.graphics.drawable.BitmapDrawable;
 
 import android.app.Dialog;
+
+import java.util.LinkedList;
 
 import fm.audioboo.data.BooData;
 
@@ -45,6 +55,30 @@ public class BooDetailsActivity extends Activity
   public static final String EXTRA_BOO_DATA = "fm.audioboo.extras.boo-data";
 
 
+  /***************************************************************************
+   * Cache baton
+   **/
+  private class Baton
+  {
+    public int containerId;
+    public int imageId;
+    public int progressId;
+
+    Baton(int _containerId, int _imageId, int _progressId)
+    {
+      containerId = _containerId;
+      imageId = _imageId;
+      progressId = _progressId;
+    }
+  }
+
+
+
+  /***************************************************************************
+   * Private data
+   **/
+  private Boo   mBoo;
+
 
   /***************************************************************************
    * Implementation
@@ -62,11 +96,11 @@ public class BooDetailsActivity extends Activity
           + EXTRA_BOO_DATA + "' extra.");
     }
 
-    Boo boo = new Boo(data);
-    Log.d(LTAG, "Data: " + data.mTitle);
-    Log.d(LTAG, "Boo: " + boo);
+    mBoo= new Boo(data);
+    Log.d(LTAG, "Boo: " + mBoo);
 
     // FIXME
+    populateView();
   }
 
 
@@ -118,5 +152,163 @@ public class BooDetailsActivity extends Activity
 //    }
 
     return dialog;
+  }
+
+
+
+  private void populateView()
+  {
+    if (null == mBoo || null == mBoo.mData) {
+      return;
+    }
+
+    // Prepare for loading images.
+    LinkedList<ImageCache.CacheItem> uris = new LinkedList<ImageCache.CacheItem>();
+
+    // Thumbnail
+    ImageView image_view = (ImageView) findViewById(R.id.boo_thumb);
+    if (null != image_view) {
+      if (null != mBoo.mData.mUser) {
+        Uri uri = mBoo.mData.mUser.getThumbUrl();
+        if (null != uri) {
+          int size = image_view.getLayoutParams().width - image_view.getPaddingLeft()
+            - image_view.getPaddingRight();
+          uris.add(new ImageCache.CacheItem(uri, size, new Baton(-1, R.id.boo_thumb, -1)));
+        }
+      }
+    }
+
+    // Author
+    TextView text_view = (TextView) findViewById(R.id.boo_author);
+    if (null != text_view) {
+      if (null != mBoo.mData.mUser && null != mBoo.mData.mUser.mUsername) {
+        text_view.setText(mBoo.mData.mUser.mUsername);
+      }
+      else {
+        text_view.setText(getResources().getString(R.string.boo_unknown_author));
+      }
+    }
+
+    // Title
+    text_view = (TextView) findViewById(R.id.boo_title);
+    if (null != text_view) {
+      text_view.setText(null != mBoo.mData.mTitle ? mBoo.mData.mTitle : "");
+    }
+
+    // Date
+    text_view = (TextView) findViewById(R.id.boo_date);
+    if (null != text_view) {
+      text_view.setText(NaturalDateFormat.format(mBoo.mData.mRecordedAt));
+    }
+
+    // Image
+    View container = findViewById(R.id.boo_image_container);
+    if (null != container) {
+      Uri image_uri = mBoo.mData.getFullUrl();
+      if (null == image_uri) {
+        container.setVisibility(View.GONE);
+      }
+      else {
+        image_view = (ImageView) container.findViewById(R.id.boo_image);
+        if (null != image_view) {
+          int size = image_view.getLayoutParams().width - image_view.getPaddingLeft()
+            - image_view.getPaddingRight();
+          uris.add(new ImageCache.CacheItem(image_uri, size,
+                new Baton(R.id.boo_image_container, R.id.boo_image, R.id.boo_image_progress)));
+        }
+      }
+    }
+
+    // Location
+    container = findViewById(R.id.boo_location_container);
+    if (null != container) {
+      if (null == mBoo.mData.mLocation) {
+        container.setVisibility(View.GONE);
+      }
+      else {
+        // Text
+        text_view = (TextView) container.findViewById(R.id.boo_location_text);
+        if (null != text_view) {
+          text_view.setText(mBoo.mData.mLocation.mDescription);
+        }
+
+        // Image
+        Uri uri = Uri.parse(String.format("http://maps.google.com/maps/api/staticmap?center=%f,%f&zoom=11&size=300x300&maptype=roadmap&markers=%f,%f&sensor=true",
+              mBoo.mData.mLocation.mLatitude, mBoo.mData.mLocation.mLongitude,
+              mBoo.mData.mLocation.mLatitude, mBoo.mData.mLocation.mLongitude));
+        image_view = (ImageView) container.findViewById(R.id.boo_location);
+        if (null != image_view) {
+          int size = image_view.getLayoutParams().width - image_view.getPaddingLeft()
+            - image_view.getPaddingRight();
+          uris.add(new ImageCache.CacheItem(uri, size,
+                new Baton(R.id.boo_location_container, R.id.boo_location, R.id.boo_location_progress)));
+
+          // Launch a location query
+          image_view.setOnClickListener(new View.OnClickListener() {
+              public void onClick(View v)
+              {
+                Uri data = Uri.parse(String.format("http://maps.google.com/?ll=%f,%f&z=11",
+                    mBoo.mData.mLocation.mLatitude, mBoo.mData.mLocation.mLongitude));
+                Intent i = new Intent(Intent.ACTION_VIEW, data);
+                startActivity(i);
+              }
+          });
+        }
+      }
+    }
+
+
+    // Finally, fire off requests for images.
+    if (0 < uris.size()) {
+      Globals.get().mImageCache.fetch(uris, new Handler(new Handler.Callback() {
+        public boolean handleMessage(Message msg)
+        {
+          ImageCache.CacheItem item = (ImageCache.CacheItem) msg.obj;
+          Baton baton = (Baton) item.mBaton;
+
+          ImageView image_view = (ImageView) findViewById(baton.imageId);
+          if (null == image_view) {
+            Log.d(LTAG, "did not find view with id: " + baton.imageId);
+            return true;
+          }
+
+          switch (msg.what) {
+            case ImageCache.MSG_OK:
+              image_view.setImageDrawable(new BitmapDrawable(item.mBitmap));
+
+              // FIXME do we always want this?
+              ViewGroup.LayoutParams params = image_view.getLayoutParams();
+              params.height = item.mBitmap.getScaledHeight(getResources().getDisplayMetrics());
+              image_view.setLayoutParams(params);
+
+              image_view.setVisibility(View.VISIBLE);
+
+              if (-1 != baton.progressId) {
+                View progress = findViewById(baton.progressId);
+                if (null != progress) {
+                  progress.setVisibility(View.GONE);
+                }
+              }
+              break;
+
+            case ImageCache.MSG_ERROR:
+            default:
+              Log.e(LTAG, "Error fetching image at URL: " + (item != null ? item.mImageUri : null));
+
+            case ImageCache.MSG_CANCELLED:
+              // Hide container
+              if (-1 != baton.containerId) {
+                View container = findViewById(baton.containerId);
+                if (null != container) {
+                  container.setVisibility(View.GONE);
+                }
+              }
+              break;
+          }
+          return true;
+        }
+      }));
+
+    }
   }
 }
