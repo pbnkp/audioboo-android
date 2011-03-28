bd66b4777ba77f9efa611125eb5e84cab8f157be
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-03-28 22:19:32 +0100

    Greatly simplify SpectralView; also smoothes animations (yay!), and makes the grid width configurable.

--- src/fm/audioboo/widget/SpectralView.java
+++ src/fm/audioboo/widget/SpectralView.java
@@ -54,32 +54,19 @@ public class SpectralView extends RelativeLayout
   // Default animation duration and FPS.
   private static final int    DEFAULT_ANIMATION_MAX_FPS   = 15;
 
-  // Default tint color for inactive views is 50% black
-  private static final int    DEFAULT_INACTIVE_TINT_COLOR = Color.argb(127, 0, 0, 0);
-
-  // Minimum/maximum bar exponent.
-  private static final float  MIN_BAR_EXP                 = 0.2f;
-  private static final float  MAX_BAR_EXP                 = 0.5f;
-
   // Low pass filter limits
-  private static final float  FILTER_LOWER_LIMIT          = 0.1f;
-  private static final float  FILTER_UPPER_LIMIT          = 0.7f;
+  private static final double FILTER_LOWER_LIMIT          = 0.1;
+  private static final double FILTER_UPPER_LIMIT          = 0.7;
+
 
   /***************************************************************************
    * Data members
    **/
   // Number of bars to display.
   private int                 mNumberOfBars;
-  // Drawable for the frame to overlay over the bars.
-  private Drawable            mFrameDrawable;
-  // Drawable for displaying when the view is inactive, i.e. startAnimation has
-  // not been called.
-  private Drawable            mInactiveDrawable;
-  // Tint color for inactive views
-  private ColorStateList      mInactiveTintColor;
+
   // Drawables for even and odd bars.
-  private ClipDrawable        mEvenBarDrawable;
-  private ClipDrawable        mOddBarDrawable;
+  private ClipDrawable        mBarDrawable;
   // Color for the grid
   private ColorStateList      mGridColor;
   // Padding for the bars & grid.
@@ -87,27 +74,28 @@ public class SpectralView extends RelativeLayout
   private int                 mBarPaddingRight;
   private int                 mBarPaddingTop;
   private int                 mBarPaddingBottom;
+  // Grid spacing
+  private int                 mGridSpacing;
 
   // Peak and average amplitude set via setAmplitudes.
   private float               mAverageAmp;
   private float               mPeakAmp;
 
-  // Animation max FPS.
-  private int                 mAnimationMaxFPS;
-  // Last draw time, needded to not exceed FPS.
-  private long                mLastDrawTime;
+  // Animation FPS.
+  private double              mFPS;
+  private int                 mFrames;
+  private long                mFrameTimestamp;
+
   // Only animate if this flag is set.
   private boolean             mShouldAnimate;
   // Factor/exponent for the slot selected via mBarHeightSlot/mBarFilterSlot
-  private float[]             mBarHeightExponents;
-  private float[]             mBarFilterFactors;
+  private double[]            mBarHeightExponents;
+  private double[]            mBarFilterFactors;
+  private double[]            mBarHeights;
   private int[]               mBarHeightSlot;
   private int[]               mBarFilterSlot;
   // Depends on limits of the filter and number of bars.
-  private float               mFilterStep;
-  // Randomly change a bar's filter/height slot every MaxFPS steps. Need to
-  // count frames for that.
-  private int                 mFrameCount;
+  private double              mFilterStep;
 
   // Context
   private Context             mContext;
@@ -156,6 +144,7 @@ public class SpectralView extends RelativeLayout
   public void startAnimation()
   {
     mShouldAnimate = true;
+    mFrameTimestamp = System.currentTimeMillis();
     invalidate();
   }
 
@@ -171,37 +160,6 @@ public class SpectralView extends RelativeLayout
 
 
 
-  @Override
-  protected void onFinishInflate()
-  {
-    super.onFinishInflate();
-
-    RelativeLayout content = (RelativeLayout) inflate(mContext, R.layout.spectral_view, this);
-
-    // Set frame drawable, if we know it.
-    if (null != mFrameDrawable) {
-      ImageView image_view = (ImageView) content.findViewById(R.id.spectral_view_frame);
-      if (null != image_view) {
-        image_view.setImageDrawable(mFrameDrawable);
-      }
-    }
-  }
-
-
-
-  private int clamp(int value, int min, int max)
-  {
-    if (value < min) {
-      return min;
-    }
-    else if (value > max) {
-      return max;
-    }
-    return value;
-  }
-
-
-
   private void initWithAttrs(AttributeSet attrs)
   {
     TypedArray a = mContext.obtainStyledAttributes(attrs, R.styleable.SpectralView);
@@ -213,47 +171,34 @@ public class SpectralView extends RelativeLayout
       throw new IllegalArgumentException("Expect numberOfBars to be at least 1.");
     }
 
-    // Determine bar height factors, based on the number of bars. If the number
-    // of bars is odd, the middle one will reach to MAX_BAR_EXP, otherwise the
-    // two sharing the center will. The left and rightmost bar will not be
-    // treated to any exponent.
-    mBarHeightExponents = new float[mNumberOfBars];
-    int idx2 = mNumberOfBars / 2;
-    int idx1 = idx2 - 1;
-    int mid_idx = -1;
-    if (0 != mNumberOfBars % 2) {
-      mid_idx = idx2;
-      idx2 += 1;
-    }
-    idx1 = clamp(idx1, 0, mNumberOfBars - 1);
-    idx2 = clamp(idx2, 0, mNumberOfBars - 1);
+    // Remember bar heights.
+    mBarHeights = new double[mNumberOfBars];
 
-    float exp = MIN_BAR_EXP;
-    float step_diff = (MAX_BAR_EXP - MIN_BAR_EXP) / idx1;
-    if (-1 != mid_idx) {
-      mBarHeightExponents[mid_idx] = exp;
-      exp += step_diff;
-    }
+    // Determine bar height factors, based on the number of bars.
+    mBarHeightExponents = new double[mNumberOfBars];
 
-    for ( ; idx1 > 0 ; --idx1, ++idx2) {
-      mBarHeightExponents[idx1] = exp;
-      mBarHeightExponents[idx2] = exp;
-      exp += step_diff;
-    }
+    final double base = 1.8; // experimentally deducded
+    final double max_x = (mNumberOfBars - 1) / 2.0;
+    final double max_y = Math.pow(base, max_x);
 
-    mBarHeightExponents[0] = 1.0f;
-    mBarHeightExponents[mNumberOfBars - 1] = 1.0f;
+    final double min = 0.2; // from iOS code
+    final double scale = 1.0 - min;
+
+    for (int i = 0 ; i < mNumberOfBars ; ++i) {
+      double x = Math.abs(i - max_x);
+      mBarHeightExponents[i] = min + (Math.pow(base, x) * scale);
+    }
 
     // Also compute factors for the low pass filter. We start at 1, work our
     // way up to (mNumberOfBars / 2) + 1 and then work our way down again
-    mBarFilterFactors = new float[mNumberOfBars];
-    float next = 1.0f;
-    float step = 1.0f;
-    for (int i = 0 ; i < mBarFilterFactors.length ; ++i) {
+    mBarFilterFactors = new double[mNumberOfBars];
+    double next = 1.0;
+    double step = 1.0;
+    for (int i = 0 ; i < mNumberOfBars ; ++i) {
       mBarFilterFactors[i] = next;
 
       if (next >= (mNumberOfBars / 2) + 1) {
-        step = -1.0f;
+        step = -1.0;
       }
       next += step;
     }
@@ -269,27 +214,12 @@ public class SpectralView extends RelativeLayout
       mBarFilterSlot[i] = mBarHeightSlot[i] = i;
     }
 
-
-    // Read frame drawable
-    mFrameDrawable = a.getDrawable(R.styleable.SpectralView_frameDrawable);
-
     // Determine drawables for even/odd bars
-    Drawable even = a.getDrawable(R.styleable.SpectralView_evenBarDrawable);
-    Drawable odd = a.getDrawable(R.styleable.SpectralView_oddBarDrawable);
-
-    if (null == even || null == odd) {
-      even = new ColorDrawable(Color.BLUE);
-      odd = new ColorDrawable(Color.GREEN);
+    Drawable draw = a.getDrawable(R.styleable.SpectralView_barDrawable);
+    if (null == draw) {
+      draw = new ColorDrawable(Color.BLUE);
     }
-    else if (null == even) {
-      even = odd;
-    }
-    else if (null == odd) {
-      odd = even;
-    }
-    mEvenBarDrawable = new ClipDrawable(even,
-        Gravity.FILL_HORIZONTAL | Gravity.BOTTOM, ClipDrawable.VERTICAL);
-    mOddBarDrawable = new ClipDrawable(odd,
+    mBarDrawable = new ClipDrawable(draw,
         Gravity.FILL_HORIZONTAL | Gravity.BOTTOM, ClipDrawable.VERTICAL);
 
     // Read padding
@@ -299,16 +229,11 @@ public class SpectralView extends RelativeLayout
     mBarPaddingTop = a.getDimensionPixelOffset(R.styleable.SpectralView_barPaddingTop, padding);
     mBarPaddingBottom = a.getDimensionPixelOffset(R.styleable.SpectralView_barPaddingBottom, padding);
 
-    // Read max animation FPS.
-    mAnimationMaxFPS = a.getInt(R.styleable.SpectralView_barAnimationMaxFPS,
-        DEFAULT_ANIMATION_MAX_FPS);
+    // Read grid spacing; default 2.
+    mGridSpacing = a.getDimensionPixelOffset(R.styleable.SpectralView_gridSpacing, 2);
 
     // Get grid color
     mGridColor = a.getColorStateList(R.styleable.SpectralView_gridColor);
-
-    // Get inactive drawable & tint color
-    mInactiveDrawable = a.getDrawable(R.styleable.SpectralView_inactiveDrawable);
-    mInactiveTintColor = a.getColorStateList(R.styleable.SpectralView_inactiveTintColor);
 
     a.recycle();
   }
@@ -320,21 +245,38 @@ public class SpectralView extends RelativeLayout
   {
     super.onDraw(canvas);
 
+    // Calculate FPS. Also calculate whether we want to permutate the height/filter
+    // functions.
+    boolean permutate = false;
+    ++mFrames;
+    long timestamp = System.currentTimeMillis();
+    long diff = timestamp - mFrameTimestamp;
+    if (diff > 1000) {
+      mFPS = mFrames / (diff / 1000.0);
+      mFrames = 0;
+      mFrameTimestamp = timestamp;
+
+      // Alright, "calculating" whether to permutate is a tiny bit of an
+      // exaggeration.
+      permutate = true;
+    }
+
+    // Log.d(LTAG, "FPS: " + mFPS);
+
     // Permutate height/filter slots every half second
-    ++mFrameCount;
-    if (0 == mFrameCount % (mAnimationMaxFPS / 2)) {
-      // Randomly pick a bar whose height exponent we change randomly. Ignore
-      // the middle bar (if any)
-      int idx = (int) (Math.random() * (mNumberOfBars - 1));
-      if (0 == mNumberOfBars % 2 || (int) (mNumberOfBars / 2) != idx) {
-        mBarHeightSlot[idx] = (int) (Math.random() * (mNumberOfBars - 1));
+    if (permutate) {
+      int middle = (mNumberOfBars - 1) / 2;
+
+      // Randomly permutate the filter function of any but the middle bar.
+      int idx = (int) (Math.random() % mNumberOfBars);
+      if (idx != middle) {
+        mBarFilterSlot[idx] = (int) (Math.random() % mNumberOfBars);
       }
 
-      // Randomly pick a bar whose filter factor we change randomly. Ignore
-      // the middle bar (if any)
-      idx = (int) (Math.random() * (mNumberOfBars - 1));
-      if (0 == mNumberOfBars % 2 || (int) (mNumberOfBars / 2) != idx) {
-        mBarFilterSlot[idx] = (int) (Math.random() * (mNumberOfBars - 1));
+      // Randomly permutate the height function of any but the middle bar
+      idx = (int) (Math.random() % mNumberOfBars);
+      if (idx != middle) {
+        mBarHeightSlot[idx] = (int) (Math.random() % mNumberOfBars);
       }
     }
 
@@ -349,20 +291,17 @@ public class SpectralView extends RelativeLayout
 
 
     // Draw the bars
+    ClipDrawable d = mBarDrawable;
     for (int i = 0 ; i < mNumberOfBars ; ++i, x += bar_width) {
-      ClipDrawable d = mEvenBarDrawable;
-      if (0 != i % 2) {
-        d = mOddBarDrawable;
-      }
-
-      float scale = 1.0f;
+      double scale = 0.0;
       if (mShouldAnimate) {
         // The scale takes into consideration the height of each bar (via their
         // exponents), and a low pass filter is applied.
-        scale = (float) Math.pow(mPeakAmp, mBarHeightExponents[mBarHeightSlot[i]]);
-        float filterSize = FILTER_LOWER_LIMIT + mBarFilterFactors[mBarFilterSlot[i]] * mFilterStep;
-        scale = filterSize * scale + (1 - filterSize) * mAverageAmp;
+        scale = Math.pow(mPeakAmp, mBarHeightExponents[mBarHeightSlot[i]]);
+        double filterSize = FILTER_LOWER_LIMIT + mBarFilterFactors[mBarFilterSlot[i]] * mFilterStep;
+        scale = filterSize * scale + (1.0 - filterSize) * mBarHeights[i];
       }
+      mBarHeights[i] = scale;
 
       // Levels are from 0 to MAX_LEVEL. In orde to show at least one pixel, we need
       // to ensure we set the level to least MAX_LEVEL/height.
@@ -378,47 +317,7 @@ public class SpectralView extends RelativeLayout
       d.draw(canvas);
     }
 
-    // Draw the inactive background, if necessary.
-    if (!mShouldAnimate) {
-      if (null != mInactiveDrawable) {
-        int dwidth = mInactiveDrawable.getIntrinsicWidth();
-        int dheight = mInactiveDrawable.getIntrinsicHeight();
-
-        int xoffs = mBarPaddingLeft;
-        int yoffs = mBarPaddingTop;
-        if (dwidth >= width || dheight >= height) {
-          int maxwidth = width - 2 * (mBarPaddingTop + mBarPaddingBottom);
-          int maxheight = height - 2 * (mBarPaddingLeft + mBarPaddingRight);
-
-          float wfactor = (float) maxwidth / dwidth;
-          float hfactor = (float) maxheight / dheight;
-          float factor = (float) Math.min(wfactor, hfactor);
-
-          dwidth *= factor;
-          dheight *= factor;
-        }
-        xoffs += (width - dwidth) / 2;
-        yoffs += (height - dheight) / 2;
-
-        mInactiveDrawable.setBounds(xoffs, yoffs, xoffs + dwidth, yoffs + dheight);
-        mInactiveDrawable.draw(canvas);
-      }
-
-      // Tint bars
-      int currentColor = DEFAULT_INACTIVE_TINT_COLOR;
-      if (null != mInactiveTintColor) {
-        currentColor = mInactiveTintColor.getColorForState(getDrawableState(),
-            DEFAULT_INACTIVE_TINT_COLOR);
-      }
-      ClipDrawable cd = new ClipDrawable(new ColorDrawable(currentColor),
-          Gravity.FILL, ClipDrawable.VERTICAL);
-
-      cd.setBounds(0, 0, getWidth(), getHeight());
-      cd.setLevel(MAX_LEVEL);
-      cd.draw(canvas);
-    }
-
-    // Draw grid over the bars. We need to draw a 2px line between bars.
+    // Draw grid over the bars. We need to draw a line between bars.
     int currentColor = Color.BLACK;
     if (null != mGridColor) {
       currentColor = mGridColor.getColorForState(getDrawableState(), Color.BLACK);
@@ -426,35 +325,18 @@ public class SpectralView extends RelativeLayout
     ClipDrawable cd = new ClipDrawable(new ColorDrawable(currentColor),
         Gravity.FILL, ClipDrawable.VERTICAL);
 
-    x = mBarPaddingLeft + (bar_width - 1);
+    x = mBarPaddingLeft + (bar_width - (mGridSpacing / 2));
     for (int i = 0 ; i < (mNumberOfBars - 1) ; ++i, x += bar_width) {
-      cd.setBounds((int) x, 0, (int) x + 2, getHeight());
+      cd.setBounds((int) x, 0, (int) x + mGridSpacing, getHeight());
       cd.setLevel(MAX_LEVEL);
       cd.draw(canvas);
     }
-    y = mBarPaddingTop + (bar_width - 1);
+    y = mBarPaddingTop + (bar_width - (mGridSpacing / 2));
     int numSquares = (int) (height / bar_width);
     for ( ; y < height ; y += bar_width) {
-      cd.setBounds(0, (int) y, getWidth(), (int) y + 2);
+      cd.setBounds(0, (int) y, getWidth(), (int) y + mGridSpacing);
       cd.setLevel(MAX_LEVEL);
       cd.draw(canvas);
-    }
-
-    // If the bars should animate, schedule the next frame. We're trying to
-    // schedule this no more often than mAnimationMaxFPS dictates.
-    if (mShouldAnimate) {
-      long frame_delay = (long) (1000.0 / mAnimationMaxFPS);
-      long current = System.currentTimeMillis();
-      long diff = current - mLastDrawTime;
-
-      if (diff >= frame_delay) {
-        mLastDrawTime = current;
-        frame_delay = (2 * frame_delay) - diff;
-        if (frame_delay < 0) {
-          frame_delay = 0;
-        }
-        postInvalidateDelayed(frame_delay);
-      }
     }
   }
 }
