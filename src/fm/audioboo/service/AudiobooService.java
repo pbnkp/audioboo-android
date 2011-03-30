90fe34ed204cfcc69a5899923e13aa54bc06a0ab
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-03-30 18:42:48 +0100

    Don't show playback notification when playback is paused.

--- src/fm/audioboo/service/AudiobooService.java
+++ src/fm/audioboo/service/AudiobooService.java
@@ -161,6 +161,52 @@ public class AudiobooService
   }
 
 
+
+  private void showPlaybackNotification()
+  {
+    PlayerState state = mPlayer.getPlayerState();
+    if (null == state) {
+      return;
+    }
+
+    // Create notification
+    NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+    // Create intent for notification clicks. We want to open the Boo's detail
+    // view.
+    Intent intent = new Intent(Intent.ACTION_VIEW, UriUtils.createDetailsUri(
+          state.mBooId, state.mBooIsMessage));
+    PendingIntent contentIntent = PendingIntent.getActivity(AudiobooService.this, 0, intent, 0);
+
+    // Create Notification
+    Resources res = getResources();
+    String title = (null != state.mBooTitle) ? state.mBooTitle : res.getString(R.string.notification_default_title);
+    String username = (null != state.mBooUsername) ? state.mBooUsername : res.getString(R.string.notification_unknown_user);
+    String text = String.format(res.getString(R.string.notification_text), username);
+    Notification notification = new Notification(R.drawable.notification,
+        null, System.currentTimeMillis());
+    notification.setLatestEventInfo(AudiobooService.this, title, text, contentIntent);
+
+    // Set ongoing and no-clear flags to ensure the notification stays until
+    // cleared by this service.
+    notification.flags |= Notification.FLAG_ONGOING_EVENT;
+    notification.flags |= Notification.FLAG_NO_CLEAR;
+
+    // Install notification
+    nm.notify(NOTIFICATION_PLAYING_BACK, notification);
+
+  }
+
+
+
+  private void cancelPlaybackNotification()
+  {
+    NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    nm.cancel(NOTIFICATION_PLAYING_BACK);
+  }
+
+
+
   /***************************************************************************
    * ProgressListener implementation
    **/
@@ -182,32 +228,7 @@ public class AudiobooService
     public void play(BooData boo, boolean playImmediately)
     {
       mPlayer.play(new Boo(boo), playImmediately);
-
-      // Create notification
-      NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-      // Create intent for notification clicks. We want to open the Boo's detail
-      // view.
-      Intent intent = new Intent(Intent.ACTION_VIEW, UriUtils.createDetailsUri(
-            boo.mId, boo.mIsMessage));
-      PendingIntent contentIntent = PendingIntent.getActivity(AudiobooService.this, 0, intent, 0);
-
-      // Create Notification
-      Resources res = getResources();
-      String title = (null != boo.mTitle) ? boo.mTitle : res.getString(R.string.notification_default_title);
-      String username = (null != boo.mUser && null != boo.mUser.mUsername) ? boo.mUser.mUsername : res.getString(R.string.notification_unknown_user);
-      String text = String.format(res.getString(R.string.notification_text), username);
-      Notification notification = new Notification(R.drawable.notification,
-          null, System.currentTimeMillis());
-      notification.setLatestEventInfo(AudiobooService.this, title, text, contentIntent);
-
-      // Set ongoing and no-clear flags to ensure the notification stays until
-      // cleared by this service.
-      notification.flags |= Notification.FLAG_ONGOING_EVENT;
-      notification.flags |= Notification.FLAG_NO_CLEAR;
-
-      // Install notification
-      nm.notify(NOTIFICATION_PLAYING_BACK, notification);
+      showPlaybackNotification();
     }
 
 
@@ -215,10 +236,7 @@ public class AudiobooService
     public void stop()
     {
       mPlayer.stopPlaying();
-
-      // Cancel notification
-      NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-      nm.cancel(NOTIFICATION_PLAYING_BACK);
+      cancelPlaybackNotification();
     }
 
 
@@ -226,6 +244,7 @@ public class AudiobooService
     public void pause()
     {
       mPlayer.pausePlaying();
+      cancelPlaybackNotification();
     }
 
 
@@ -233,6 +252,7 @@ public class AudiobooService
     public void resume()
     {
       mPlayer.resumePlaying();
+      showPlaybackNotification();
     }
 
 
